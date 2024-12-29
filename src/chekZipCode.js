1f4343b679aa21ae9bac3aa021834a6a3cdac293
Axyl07 <aryanchauhan000777@gmail.com>
2024-12-29 22:55:40 +0530

    Email and zipcodes validation done

---
+++ src/chekZipCode.js
@@ -0,0 +1,28 @@
+export default function checkZipCode() {
+  const constraints = {
+    USA: [
+      "^(U-)?\\d{5}$",
+      "US postal codes must have exactly 5 digits: e.g. U-75012 or 75012",
+    ],
+    India: [
+      "^(I-)?\\d{6}$",
+      "Indian postal codes must have exactly 6 digits: e.g. I-75012 or 75012",
+    ],
+    Japan: [
+      "^(J-)?\\d{7}$",
+      "Indian postal codes must have exactly 7 digits: e.g. I-75012 or 75012",
+    ],
+  };
+    const country = document.getElementById('country').value;
+    const zipCode = document.querySelector('#zipCode');
+    const zipError = document.querySelector('#zipError');
+    const constraint = new RegExp(constraints[country][0], "");
+    console.log(constraint);
+    if (constraint.test(zipCode.value)) {
+        zipCode.setCustomValidity("");
+        zipError.textContent = ""
+    } else {
+        zipCode.setCustomValidity(constraints[country][1]);
+        zipError.textContent = constraints[country][1];
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,50 @@
-import test from "./test";
-test();
+import checkZipCode from "./chekZipCode";
+
+
+window.onload = () => {
+    document.getElementById("country").onchange = checkZipCode;
+    document.getElementById("zipCode").oninput = checkZipCode;
+  };
+  
+
+const form = document.querySelector('form');
+const email = document.querySelector('#email');
+const emailError = document.querySelector('#emailError');
+// const country = document.querySelector('#country');
+// const zipCode = document.querySelector('#zipCode');
+// const password = document.querySelector('#password');
+// const confirm = document.querySelector('#confirm');
+// const submitButton = document.querySelector('#submit');
+
+
+email.addEventListener('input', () => {
+  if (email.validity.valid) {
+    emailError.textContent = "";
+  } else {
+    showError();
+  }
+});
+
+form.addEventListener("submit", (event) => {
+    
+  // if the email field is invalid
+  if (!email.validity.valid) {
+    // display an appropriate error message
+    showError();
+    // prevent form submission
+    event.preventDefault();
+  }
+});
+
+const showError = function () {
+  if (email.validity.valueMissing) {
+    // If empty
+    emailError.textContent = "You need to enter an email address.";
+  }  if (email.validity.typeMismatch) {
+    // If it's not an email address,
+    emailError.textContent = "Entered value needs to be an email address. yahoo";
+  } else  if (email.validity.tooShort) {
+    // If the value is too short,
+    emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+  }
+}
